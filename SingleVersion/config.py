0c257f6c0c43bdc2d679c3c22ca5db38b5bdb324
your_github_username <drewsteindl@icloud.com>
2025-03-26 14:51:44 -0400

    Added Noise + Functionality for 6_2

--- SingleVersion/config.py
+++ SingleVersion/config.py
@@ -1,18 +1,16 @@
-# config.py
-
 # Training parameters
 num_iterations = 1          # Total number of training iterations
-num_episodes = 100             # Number of self-play games per iteration
-num_mcts_sims = 25             # Number of MCTS simulations per move
-learning_rate = 0.001          # Learning rate for the neural network
-checkpoint_path = './models/'  # Directory to save model checkpoints
-load_model = False             # Whether to load an existing model (used for evaluation)
-load_model_file = 'best.pth.tar'  # File name of the model to load
-save_model_freq = 100          # Save the model every N iterations
-
+num_episodes = 100          # Number of self-play games per iteration
+num_mcts_sims = 25          # Number of MCTS simulations per move
+learning_rate = 0.001       # Learning rate for the neural network
+num_epochs = 10             # 🔥 NEW: Number of epochs per training step
+checkpoint_path = './models/'
+load_model = False
+load_model_file = 'best.pth.tar'
+save_model_freq = 100
 # Existing parameters
-knotter_first = False          # If True, the knotter (Player -1) goes first
-n_games = 100                  # Number of games to play for evaluation scripts
+knotter_first = False
+n_games = 100
 
 # New parameter
-resumeTraining = True         # If True, resume training from the current model
+resumeTraining = False
